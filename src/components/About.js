d5d1c73382464fb379e412177975728aff9a5bfe
Paul Donovan <paul.m.donovan@outlook.com>
2024-05-25 12:37:36 +1000

    provided code solution

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <h3>I'm an aspiring web developer. I have an eye for detail whislt also making the most of the technical side of your website to ensure an outstanding customer experiecne.</h3>
+    <img src={image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
